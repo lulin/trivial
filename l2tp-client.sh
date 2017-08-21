bcbd141b6f544b4c0ab37279e2b0494d1fc90db8
lulin.wang <lulin.wang@phicomm.com>
2017-08-21 17:13:23 +0800

    l2

--- l2tp-client.sh
+++ l2tp-client.sh
@@ -5,9 +5,60 @@ L2TP_PIPE=/var/run/xl2tpd/l2tp-control
 CONFILE=/tmp/xl2tpd.conf
 
 start() {
-    config
-    xl2tpd -c $CONFILE
-    if
+    wan_proto=$(nvram get wan_proto)
+    vpnc_enable=$(nvram get vpnc_enable)
+    [ -z "$vpnc_enable" ] && vpnc_enable=0
+
+    if [ "$wan_proto" != "l2tp" -a $vpnc_enable -eq 0 ]; then
+        echo "Neither is WAN protocol L2TP, nor is VPN client enabled"
+        exit 1
+    fi
+
+    # config
+    > "$CONFILE"
+
+    # Add global
+    config_add_global "$CONFILE"
+
+    # setup for wan l2tp mode
+    if [ "$wan_proto" == "l2tp" ]; then
+        wan_lac="wan_l2tp"
+        wan_lns=$(nvram get wan_l2tp_server)
+        wan_optfile="/tmp/wan_l2tp.options"
+        config_add_lac $CONFILE "$wan_lac" "$wan_lns" "$wan_optfile"
+    fi
+
+    # setup for vpn l2tp client
+    if [ $vpnc_enable -ne 0 ]; then
+        vpnc_lac="vpnc_l2tp"
+        vpnc_lns=$(nvram get vpnc_name)
+        vpnc_optfile="/tmp/vpnc_l2tp.options"
+        config_add_lac $CONFILE "$vpnc_lac" "$vpnc_lns" "$vpnc_optfile"
+    fi
+
+    xl2tpd -c $CONFILE &
+
+    local count=0
+    while [ ! -p $L2TP_PIPE ]; do
+        ((count++))
+        if [ $count -gt 5 ]; then
+            echo "start xl2tpd failed, pipe $L2TP_PIPE missing"
+            exit 1
+        fi
+        sleep 1
+    done
+
+    # connect wan, if setup
+    if [ "$wan_proto" == "l2tp" ]; then
+        xl2tpd-control add $wan_lac
+        xl2tpd-control connect $wan_lac
+    fi
+
+    # connect vpn, if setup
+    if [ $vpnc_enable -ne 0 ]; then
+        xl2tpd_control add $vpnc_lac
+        xl2tpd_control connect $vpnc_lac
+    fi
 }
 
 stop() {
@@ -17,32 +68,28 @@ stop() {
 restart() {
     stop
     # wait until stopped
-    count=0
-    while [ proc_alive && test $count -lt 5 ]; do
+    local count=0
+    while [ $(proc_alive) == "true" ]; do
+        if [ $count -gt 5 ]; then
+            echo "failed to stop xl2tpd"
+            exit 1
+        fi
+
         sleep 1
         ((count++))
     done
+
     start
-    return $?
 }
 
-proc_zombie() {}
 proc_alive() {
     local proc="$1"
 
     ps |grep "$proc"|grep -v grep >/dev/null
     ret=$?
-    test $? -eq 0 && return true
-    return false
+    test $ret -eq 0 && echo true && return
+    echo false && return
 }
-
-reload() {
-    check_wan_l2tp
-    config
-    restart
-}
-
-check_wan_l2tp() {}
 
 build_optfile() {
     local optfile username password
@@ -88,16 +135,17 @@ config_add_lac() {
     lns="$3"
     optfile="$4"
     cat <<EOF >> "$confile"
-[lac $g_lac]
+[lac $lac]
 pppoptfile = $optfile
 lns = $lns
 name = default
 require authentication = no
 tunnel rws = 8
-autodial = yes
 redial = yes
 redial timeout = 15
 tx bps = 100000000
 rx bps = 100000000
 EOF
 }
+
+$1
